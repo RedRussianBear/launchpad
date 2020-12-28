2a04ea2e4899d4cd19a10137c0a8853ffa68037e
Mikhail Khrenov <mkhrenov34@gmail.com>
2020-12-27 23:34:29 -0500

    Added delays to ensure completion

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,17 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#define RELAY_PIN   5
+
 RF24 radio(7, 8);
 uint8_t readAddr[] = "UNODE";
 uint8_t writeAddr[] = "DNODE";
 byte buffer[32];
 
 void setup() {
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, LOW);
+
     Serial.begin(115200);
 
     radio.begin();
@@ -24,6 +29,7 @@ void setup() {
 
 void loop() {
     if (Serial.available()) {
+        delay(10);
         int nbytes = min(sizeof buffer, Serial.available());
         Serial.readBytes(buffer, nbytes);
 
@@ -33,9 +39,15 @@ void loop() {
     }
 
     if (radio.available()) {
+        delay(10);
         radio.read(buffer, 32);
 
+        if(memcmp(buffer, "IGNITION", (sizeof "IGNITION") - 1) == 0){
+            digitalWrite(RELAY_PIN, HIGH);
+        }
+
         Serial.write(buffer, 32);
+        Serial.println();
     }
 
 }
